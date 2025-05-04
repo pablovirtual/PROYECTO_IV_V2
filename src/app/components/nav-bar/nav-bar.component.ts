8b8717c689b0ee6bb5ec90901f042ed3a90137e0
pablovirtual <143946760+pablovirtual@users.noreply.github.com>
2025-05-03 21:13:02 -0600

    se agrega icono i favicon

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -10,7 +10,11 @@ import { AuthService } from '../../services/auth.service';
   template: `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" routerLink="/home">Trading Academy</a>
+        <a class="navbar-brand d-flex align-items-center" routerLink="/home">
+          <!-- Imagen del logotipo -->
+          <img src="assets/images/logotipo.png" alt="Trading Academy Logo" style="height: 40px; margin-right: 10px;">
+          <span>Trading Academy</span>
+        </a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" 
                 aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
